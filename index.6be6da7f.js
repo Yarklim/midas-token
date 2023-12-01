d77bed51fb0d74fb4defb2bda0cf5ad639cb3545
Yarklim <105521751+Yarklim@users.noreply.github.com>
2023-12-01 14:13:58 +0000

    Deploying to gh-pages from @ Yarklim/midas-token@0e8a49ead53034bc49c398a5ddcd6450c9f40b13 🚀

---
+++ index.6be6da7f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger-menu");e.addEventListener("click",(()=>{e.classList.toggle("is-open")}));const s=document.querySelector(".header__message");document.querySelector(".message__btn--close").addEventListener("click",(function(){s.classList.add("display-none")}));
+//# sourceMappingURL=index.6be6da7f.js.map
---
+++ index.7f727b7b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger-menu");e.addEventListener("click",(function(){e.classList.toggle("is-open")}));var n=document.querySelector(".header__message");document.querySelector(".message__btn--close").addEventListener("click",(function(){n.classList.add("display-none")}))}();
+//# sourceMappingURL=index.7f727b7b.js.map
