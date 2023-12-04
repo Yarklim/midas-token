399b8a42fca79d856aa648c590bc2a9557f7e315
Yarklim <105521751+Yarklim@users.noreply.github.com>
2023-12-04 12:48:02 +0000

    Deploying to gh-pages from @ Yarklim/midas-token@f351e6456ed6b79915229878e4b9c5a0a3833628 🚀

--- index.b5d595e2.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".header__message");document.querySelector(".message__btn--close").addEventListener("click",(function(){e.classList.add("display-none")}));
-//# sourceMappingURL=index.b5d595e2.js.map
--- index.f5647f46.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".header__message");document.querySelector(".message__btn--close").addEventListener("click",(function(){e.classList.add("display-none")}))}();
-//# sourceMappingURL=index.f5647f46.js.map
