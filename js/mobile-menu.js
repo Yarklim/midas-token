ede6c1e7fb413df93557785656277b6a14afaae9
Yaroslav Klimas <yarklim@gmail.com>
2023-12-07 10:31:48 +0200

    update header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,11 +5,15 @@ const menuItems = document.querySelectorAll('.header__nav-item');
 const menuOpen = () => {
   burger.classList.add('is-open');
   menu.classList.add('open-menu');
+
+  document.body.style.overflow = 'hidden';
 };
 
 const menuClose = () => {
   burger.classList.remove('is-open');
   menu.classList.remove('open-menu');
+
+  document.body.style.overflow = '';
 };
 
 burger.addEventListener('click', () => {
