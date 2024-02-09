6075a8a546376e51e178caf39f7eeee543d5a500
Yaroslav Klimas <yarklim@gmail.com>
2024-02-09 12:35:54 +0200

    updated user-actions.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,28 @@
 import { toggleUserMenu } from './user-actions';
 
 const burgerEl = document.querySelector('.burger-menu');
-const menu = document.querySelector('.header__menu');
-const wrapper = document.querySelector('.header__wrapper');
-const menuItems = document.querySelectorAll('.header__nav-item');
+const menuEl = document.querySelector('.header__menu');
+const wrapperEl = document.querySelector('.header__wrapper');
+const menuItemsEl = document.querySelectorAll('.header__nav-item');
 const dropDownEl = document.querySelectorAll('.nav__drop-down--item');
-const userMenu = document.querySelector('.user-actions__drop-down--list');
+const userMenuEl = document.querySelector('.user-actions__drop-down--list');
 
 const menuOpen = () => {
-  if (userMenu.classList.contains('active__drop-down')) {
+  if (userMenuEl.classList.contains('active__drop-down')) {
     toggleUserMenu();
   }
 
   burgerEl.classList.add('is-open');
-  menu.classList.add('open-menu');
-  wrapper.classList.add('open-menu');
+  menuEl.classList.add('open-menu');
+  wrapperEl.classList.add('open-menu');
 
   document.body.style.overflow = 'hidden';
 };
 
-const menuClose = () => {
+export const menuClose = () => {
   burgerEl.classList.remove('is-open');
-  menu.classList.remove('open-menu');
-  wrapper.classList.remove('open-menu');
+  menuEl.classList.remove('open-menu');
+  wrapperEl.classList.remove('open-menu');
 
   document.body.style.overflow = '';
 };
@@ -35,7 +35,7 @@ burgerEl.addEventListener('click', () => {
   }
 });
 
-menuItems.forEach(item =>
+menuItemsEl.forEach(item =>
   item.addEventListener('click', () => {
     if (burgerEl.classList.value.includes('is-open')) {
       menuClose();
@@ -58,7 +58,7 @@ dropDownEl.forEach(item =>
 window.matchMedia('(min-width: 910px)').addEventListener('change', e => {
   if (!e.matches) return;
 
-  if (userMenu.classList.contains('active__drop-down')) {
+  if (userMenuEl.classList.contains('active__drop-down')) {
     toggleUserMenu();
   }
 
--- js/user-actions.js
+++ js/user-actions.js
@@ -1,24 +1,36 @@
-const userAcrtionEl = document.querySelector('.user__actions');
-const userActionBtn = document.querySelector('.nav-select-icon');
-const userMenu = document.querySelector('.user-actions__drop-down--list');
+import { menuClose } from './mobile-menu';
+
+const userActionEl = document.querySelector('.user__actions');
+const userActionBtnEl = document.querySelector('.nav-select-icon');
+const userMenuEl = document.querySelector('.user-actions__drop-down--list');
+const userMenuItemEl = document.querySelectorAll(
+  '.user-actions__drop-down--item'
+);
+const mobileMenuEl = document.querySelector('.header__menu');
 
 let isOpen = false;
 
-userAcrtionEl.addEventListener('click', toggleUserMenu);
+userActionEl.addEventListener('click', toggleUserMenu);
 
-userMenu.addEventListener('click', () => {
+userMenuEl.addEventListener('click', () => {
   isOpen && toggleUserMenu();
 });
+
+userMenuItemEl.forEach(item =>
+  item.addEventListener('click', () => {
+    if (mobileMenuEl.classList.contains('open-menu')) menuClose();
+  })
+);
 
 document.addEventListener('keydown', e => {
   if (isOpen && e.code === 'Escape') toggleUserMenu();
 });
 
 export function toggleUserMenu() {
-  userMenu.classList.toggle('active__drop-down');
-  userActionBtn.classList.toggle('active__drop-down');
+  userMenuEl.classList.toggle('active__drop-down');
+  userActionBtnEl.classList.toggle('active__drop-down');
 
-  if (userMenu.classList.contains('active__drop-down')) {
+  if (userMenuEl.classList.contains('active__drop-down')) {
     isOpen = true;
   } else {
     isOpen = false;
