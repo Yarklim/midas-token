75fc4826423dd190e120b2a5521ebfceed101a93
Yaroslav Klimas <yarklim@gmail.com>
2024-03-11 15:51:50 +0200

    updated counter

--- js/numbersCount.js
+++ js/numbersCount.js
@@ -2,7 +2,7 @@ export const doCount = (element, num, step, time, format) => {
   let startValue = 0;
   let endValue = num;
   let duration = Math.floor(time / endValue);
-  let formatter = new Intl.NumberFormat(format);
+  let formatter = new Intl.NumberFormat(...format);
   let counter = setInterval(() => {
     startValue += step;
     element.textContent = `$${formatter.format(startValue)}`;
--- js/observerAnimations.js
+++ js/observerAnimations.js
@@ -7,6 +7,12 @@ window.onload = () => {
   const countVolume = document.querySelector('[data-count-volume]');
   const countMarket = document.querySelector('[data-count-market]');
   const countSupply = document.querySelector('[data-count-supply]');
+  const countPrice = document.querySelector('[data-count-price]');
+  const countStake = document.querySelector('[data-count-stake]');
+  const countUsers = document.querySelector('[data-count-users]');
+  const countTotal = document.querySelector('[data-count-total]');
+  const countLiquidity = document.querySelector('[data-count-liquidity]');
+  const countBack = document.querySelector('[data-count-back]');
 
   const options = {
     root: null,
@@ -25,13 +31,31 @@ window.onload = () => {
           inputCheckBg.classList.add('checkedBg');
         }
         if (entry.target.hasAttribute('data-count-volume')) {
-          doCount(countVolume, 185246, 1000, 2000, 'en');
+          doCount(countVolume, 185246, 1123, 2000, ['en']);
         }
         if (entry.target.hasAttribute('data-count-market')) {
-          doCount(countMarket, 47572397, 150000, 2000, 'en');
+          doCount(countMarket, 47572397, 151232, 2000, ['en']);
         }
         if (entry.target.hasAttribute('data-count-supply')) {
-          doCount(countSupply, 2575183, 10000, 2000, 'en');
+          doCount(countSupply, 2575183, 10178, 2000, ['en']);
+        }
+        if (entry.target.hasAttribute('data-count-price')) {
+          doCount(countPrice, 19.24, 1.38, 2000, ['en-IN']);
+        }
+        if (entry.target.hasAttribute('data-count-stake')) {
+          doCount(countStake, 30, 1, 2000, ['en']);
+        }
+        if (entry.target.hasAttribute('data-count-users')) {
+          doCount(countUsers, 2600, 5, 2000, ['en']);
+        }
+        if (entry.target.hasAttribute('data-count-total')) {
+          doCount(countTotal, 1800000, 5245, 2000, ['en']);
+        }
+        if (entry.target.hasAttribute('data-count-liquidity')) {
+          doCount(countLiquidity, 4824884, 10123, 2000, ['en']);
+        }
+        if (entry.target.hasAttribute('data-count-back')) {
+          doCount(countBack, 63844, 213, 5000, ['en']);
         }
 
         const entryEl = entry.target;
